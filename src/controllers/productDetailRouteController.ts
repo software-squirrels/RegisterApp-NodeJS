5c1141479136639ada1ba9b8785970d06d8e684a
Reid Phillips <xorlogic@gmail.com>
2020-01-08 08:51:48 -0600

    Updates to the product details route controller for sprint 2 functionality, incomplete.

--- src/controllers/productDetailRouteController.ts
+++ src/controllers/productDetailRouteController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import * as Helper from "./helpers/routeControllerHelper";
 import { Resources, ResourceKey } from "../resourceLookup";
 import * as ProductQuery from "./commands/products/productQuery";
-import { ViewNameLookup, ParameterLookup, RouteLookup } from "./lookups/routingLookup";
 import * as ProductCreateCommand from "./commands/products/productCreateCommand";
 import * as ProductDeleteCommand from "./commands/products/productDeleteCommand";
 import * as ProductUpdateCommand from "./commands/products/productUpdateCommand";
-import { CommandResponse, Product, ProductDetailPageResponse, ApiResponse, ProductSaveResponse, ProductSaveRequest } from "./typeDefinitions";
+import * as ValidateActiveUser from "./commands/activeUsers/validateActiveUserCommand";
+import { ViewNameLookup, ParameterLookup, RouteLookup } from "./lookups/routingLookup";
+import { CommandResponse, Product, ProductDetailPageResponse, ApiResponse, ProductSaveResponse, ProductSaveRequest, ActiveUser } from "./typeDefinitions";
 
 const processStartProductDetailError = (res: Response, error: any): void => {
 	let errorMessage: (string | undefined) = "";
@@ -45,17 +47,33 @@ const saveProduct = async (
 	performSave: (productSaveRequest: ProductSaveRequest) => Promise<CommandResponse<Product>>
 ): Promise<void> => {
 
-	return performSave(req.body)
-		.then((createProductCommandResponse: CommandResponse<Product>): void => {
+	if (Helper.handleInvalidApiSession(req, res)) {
+		return;
+	}
+
+	return ValidateActiveUser.execute((<Express.Session>req.session).id)
+		.then((activeUserCommandResponse: CommandResponse<ActiveUser>): Promise<CommandResponse<Product>> => {
+			if (false/* TODO: Verify that the user associated with the current session is elevated or not */) {
+				return Promise.reject(<CommandResponse<Product>>{
+					status: 403,
+					message: Resources.getString(ResourceKey.USER_NO_PERMISSIONS)
+				});
+			}
+
+			return performSave(req.body);
+		}).then((createProductCommandResponse: CommandResponse<Product>): void => {
 			res.status(createProductCommandResponse.status)
 				.send(<ProductSaveResponse>{
 					product: <Product>createProductCommandResponse.data
 				});
 		}).catch((error: any): void => {
-			res.status(error.status || 500)
-				.send(<ApiResponse>{
-					errorMessage: (error.message
-						|| Resources.getString(ResourceKey.PRODUCT_UNABLE_TO_SAVE))
+			return Helper.processApiError(
+				error,
+				res,
+				<Helper.ApiErrorHints>{
+					redirectBaseLocation: RouteLookup.ProductListing,
+					defaultErrorMessage: Resources.getString(
+						ResourceKey.PRODUCT_UNABLE_TO_SAVE)
 				});
 		});
 };
@@ -69,6 +87,11 @@ export const createProduct = async (req: Request, res: Response): Promise<void> 
 };
 
 export const deleteProduct = async (req: Request, res: Response): Promise<void> => {
+	if (Helper.handleInvalidApiSession(req, res)) {
+		return;
+	}
+
+	// TODO: Verify that the user associated with the current session is elevated or not
 	return ProductDeleteCommand.execute(req.params[ParameterLookup.ProductId])
 		.then((deleteProductCommandResponse: CommandResponse<void>): void => {
 			res.status(deleteProductCommandResponse.status)
@@ -76,10 +99,13 @@ export const deleteProduct = async (req: Request, res: Response): Promise<void> 
 					redirectUrl: RouteLookup.ProductListing
 				});
 		}).catch((error: any): void => {
-			res.status(error.status || 500)
-				.send(<ApiResponse>{
-					errorMessage: (error.message
-						|| Resources.getString(ResourceKey.PRODUCT_UNABLE_TO_DELETE))
+			return Helper.processApiError(
+				error,
+				res,
+				<Helper.ApiErrorHints>{
+					redirectBaseLocation: RouteLookup.ProductListing,
+					defaultErrorMessage: Resources.getString(
+						ResourceKey.PRODUCT_UNABLE_TO_DELETE)
 				});
 		});
 };
