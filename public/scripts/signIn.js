f2dc9eee7bd12e5d19bb54b6fb6e2f953dc39c04
EmrLan <57022651+EmrLan@users.noreply.github.com>
2020-02-20 15:53:25 -0600

    Task 3: Sign In - Client-side functionality

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -4,5 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function validateForm() {
 	// TODO: Validate the user input
+	var employeeID = document.getElementById("employeeID").value;
+	var password = document.getElementById("password").value;
+	
+	if(isNaN(employeeID))
+	{
+		alert("Enter a valid ID number")
+		return false;
+	}
+
+	if(password == "")
+	{
+		alert("Enter a password")
+		return false;
+	}
+
 	return true;
 }
