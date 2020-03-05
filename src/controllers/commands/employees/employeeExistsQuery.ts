8e885caa7d3f343e5abe8c8a3a72b05963118780
bouchara0303 <austinabouchard@gmail.com>
2020-03-05 12:57:03 -0600

    Check if employee exists

---
+++ src/controllers/commands/employees/employeeExistsQuery.ts
@@ -0,0 +1,21 @@
+import { EmployeeModel } from "../models/employeeModel";
+import { Resources, ResourceKey } from "../../../resourceLookup";
+import * as EmployeeRepository from "../models/employeeModel";
+import { CommandResponse, Employee } from "../../typeDefinitions";
+import * as EmployeeHelper from "./helpers/employeeHelper";
+
+export const execute = async (): Promise<CommandResponse<Employee>> => {
+	return EmployeeRepository.queryExists()
+	.then((queriedActiveUser: (EmployeeModel | null)): Promise<CommandResponse<Employee>> => {
+		if (queriedActiveUser) {
+			return Promise.resolve(<CommandResponse<Employee>>{
+				status: 200,
+				data: EmployeeHelper.mapEmployeeData(queriedActiveUser)});
+			}
+
+			return Promise.reject(<CommandResponse<Employee>>{
+				status: 404,
+				message: Resources.getString(ResourceKey.EMPLOYEES_UNABLE_TO_QUERY)
+			});
+		});
+	};
